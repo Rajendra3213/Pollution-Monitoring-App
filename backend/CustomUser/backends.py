d21350350bd220a695454122cbef8cdf3deea2de
benup211 <benup211@gmail.com>
2024-04-17 11:51:34 +0545

    fixed:authentication
    backend authentication with email fixed with BaseBackend

--- backend/CustomUser/backends.py
+++ backend/CustomUser/backends.py
@@ -1,21 +1,17 @@
 from .models import User
 from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth import get_user_model
 
-
-class customAuth(BaseBackend):
-    def authenticate(self,email=None,password=None):
+class EmailBackend(BaseBackend):
+    def authenticate(self, request, email=None, password=None, **kwargs):
         try:
-            user=User.objects.get(email=email)
+            user = User.objects.get(email=email)
             if user.check_password(password):
                 return user
         except User.DoesNotExist:
             return None
-    def get_user(self,user_id):
+
+    def get_user(self, user_id):
         try:
-            user=User.objects.get(pk=user_id)
-            if user.is_active:
-                return user
-            return None
+            return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
