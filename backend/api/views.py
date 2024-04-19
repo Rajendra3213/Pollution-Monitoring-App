a68e33224ca3b6b58dedce9c9c2153c2b3bd50e8
benup211 <benup211@gmail.com>
2024-04-19 21:26:28 +0545

    add:refresh token
    generate access token for expired token
    through refresh token provided

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,30 +1,56 @@
-from ninja import Schema,Router
+from ninja import Router,NinjaAPI
 from CustomUser.models import User
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from ninja.security import HttpBearer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_simplejwt.tokens import RefreshToken
+from pydantic import BaseModel,EmailStr,ValidationError,field_validator,Field
+import re
+from django.utils import timezone
 
 router = Router()
 
-class RegisterIn(Schema):
-    email:str
+class RegisterIn(BaseModel):
+    email:EmailStr
     password:str
     first_name:str|None=None
     last_name:str|None=None
     phone_number:str|None=None
     address:str|None=None
 
+    @field_validator('password')
+    def validate_password(cls,value):
+        pattern=r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$'
+        if not re.match(pattern,value):
+            raise ValueError("Enter a valid password")
+        return value
+    @field_validator('phone_number')
+    def validate_number(cls,value):
+        pattern=r'\+?\d{10,}'
+        if not re.match(pattern,value):
+            raise ValueError("Enter a valid number")
+        return value
 
-class LoginIn(Schema):
-    email:str
+class LoginIn(BaseModel):
+    email:EmailStr
     password:str
+
+    @field_validator('password')
+    def validate_password(cls,value):
+        pattern=r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$'
+        if not re.match(pattern,value):
+            raise ValueError("Enter a valid password")
+        return value
+
+class RefreshIn(BaseModel):
+    refresh:str
 
 @router.post('register/')
 def registerUser(request,data:RegisterIn):
     if User.objects.filter(email=data.email).exists():
-        return {"message": "Username already exists"}
+        return {"message": "Email already exists"}
     user = User.objects.create_user(email=data.email,
         password=data.password,
         first_name=data.first_name,
@@ -47,7 +73,7 @@ class CustomHttpBearer(HttpBearer):
 def check(request):
     if request.auth is None:
         return 401
-    return {"msg":"validated"}
+    return {"msg":"Token Valid"}
 
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
@@ -61,4 +87,12 @@ def loginUser(request,logindata:LoginIn):
     user=authenticate(email=logindata.email,password=logindata.password)
     if user is not None:
         return get_tokens_for_user(user)
-    return {"msg":"unsucessful login"}
+    return {"msg":"User does not exist"}
+
+@router.post('token/refresh/')
+def tokenRefresh(request,data:RefreshIn):
+    checkToken=RefreshToken(data.refresh,verify=False)
+    is_expired=checkToken.get('exp')<timezone.now().timestamp()
+    if not is_expired:
+        return NinjaAPI().create_response(request,{"access":str(checkToken.access_token)},status=201)
+    return NinjaAPI().create_response(request,{"detail":"token is expired"},status=401)
